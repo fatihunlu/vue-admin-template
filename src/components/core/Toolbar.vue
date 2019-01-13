7f4710ad64f7f4e27138e07e57005c40fca187d2
Fatih Ünlü <unlu.fa@gmail.com>
2019-01-13 08:59:26 +0300

    notifications added

--- src/components/core/Toolbar.vue
+++ src/components/core/Toolbar.vue
@@ -6,14 +6,14 @@
     <v-toolbar-title>
       <v-toolbar-side-icon @click="toggleNavigationBar"></v-toolbar-side-icon>
     </v-toolbar-title>
-      <v-text-field
-        flat
-        solo-inverted
-        append-icon="search"
-        label="Search">
-      </v-text-field>
-      <v-spacer></v-spacer>
-      <v-rating
+    <v-text-field
+      flat
+      solo-inverted
+      append-icon="search"
+      label="Search">
+    </v-text-field>
+    <v-spacer></v-spacer>
+    <v-rating
         v-model="rating"
         background-color="white"
         medium
@@ -141,35 +141,73 @@
       {{ result }}
     </v-snackbar>
 
-      <v-btn icon href="https://github.com/fatihunlu" :ripple="false">
-        <v-icon medium>fab fa-github</v-icon>
+    <v-btn icon href="https://github.com/fatihunlu" :ripple="false">
+      <v-icon medium>fab fa-github</v-icon>
+    </v-btn>
+    <v-menu offset-y origin="center center" :nudge-bottom="10" transition="scale-transition">
+
+      <v-btn icon flat slot="activator" @click="notifications.map(x => x.isActive = false)">
+        <v-badge color="green" overlap>
+          <span slot="badge" v-if="notifications.filter(x => x.isActive).length > 0">
+             {{ notifications.filter(x => x.isActive).length }}
+          </span>
+          <v-icon medium>notifications</v-icon>
+        </v-badge>
       </v-btn>
-      <v-menu offset-y origin="center center" :nudge-bottom="10" transition="scale-transition">
-        <v-btn icon large flat slot="activator" :ripple="false">
-          <v-avatar size="42px">
-            <img src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Sunglasses&hairColor=Black&facialHairType=Blank&clotheType=CollarSweater&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Light"/>
-          </v-avatar>
-        </v-btn>
-        <v-list>
-          <v-list-tile
-            v-for="(item,index) in items"
-            :key="index"
-            :to="!item.href ? { name: item.name } : null"
-            :href="item.href"
-            ripple="ripple"
-            :disabled="item.disabled"
-            :target="item.target"
-            rel="noopener"
-            @click="item.click">
-            <v-list-tile-action v-if="item.icon">
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
+
+      <v-card class="elevation-0">
+        <v-toolbar card dense color="transparent">
+          <v-toolbar-title><h5>You have {{ notifications.length }} new notification(s)</h5></v-toolbar-title>
+        </v-toolbar>
+        <v-card-text class="pa-0">
+          <v-list two-line class="pa-0">
+          <template v-for="(item, index) in notifications">
+            <v-divider :key="index" />
+            <v-list-tile avatar :key="item.title" @click.parent="item.onClick">
+            <v-list-tile-avatar :color="item.color">
+              <v-icon dark>{{item.icon}}</v-icon>
+            </v-list-tile-avatar>
             <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              <v-list-tile-sub-title v-html="item.title"></v-list-tile-sub-title>
             </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-menu>
+            <v-list-tile-action class="caption">
+              {{item.actionAt}}
+            </v-list-tile-action>
+            </v-list-tile>
+          </template>
+          </v-list>
+          <v-divider></v-divider>
+          <v-btn block flat v-if="false">See all notifications</v-btn>
+          <v-divider></v-divider>
+        </v-card-text>
+      </v-card>
+    </v-menu>
+
+    <v-menu offset-y origin="center center" :nudge-bottom="10" transition="scale-transition">
+      <v-btn icon large flat slot="activator" :ripple="false">
+        <v-avatar size="42px">
+          <img src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Sunglasses&hairColor=Black&facialHairType=Blank&clotheType=CollarSweater&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Smile&skinColor=Light"/>
+        </v-avatar>
+      </v-btn>
+      <v-list>
+        <v-list-tile
+          v-for="(item,index) in items"
+          :key="index"
+          :to="!item.href ? { name: item.name } : null"
+          :href="item.href"
+          ripple="ripple"
+          :disabled="item.disabled"
+          :target="item.target"
+          @click="item.click">
+          <v-list-tile-action v-if="item.icon">
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
   </v-toolbar>
 </template>
 <script>
@@ -215,6 +253,45 @@ export default {
             const vm = this;
 
             vm.$router.push({ name: 'Login' });
+          }
+        }
+      ],
+      notifications:
+      [
+        {
+          title: 'New mail from Adam Joe',
+          color: 'light-blue',
+          icon: 'email',
+          actionAt: '12 min ago',
+          isActive: true,
+          onClick: () => {
+            const vm = this;
+
+            vm.$router.push({ name: 'Mailbox' });
+          }
+        },
+        {
+          title: 'Scheculed meeting',
+          color: 'red',
+          icon: 'calendar_today',
+          actionAt: '46 min ago',
+          isActive: true,
+          onClick: () => {
+            const vm = this;
+
+            vm.$router.push({ name: 'Calendar' });
+          }
+        },
+        {
+          title: 'New mail from Github',
+          color: 'light-blue',
+          icon: 'email',
+          isActive: true,
+          timeLabel: '2 hour ago',
+          onClick: () => {
+            const vm = this;
+
+            vm.$router.push({ name: 'Mailbox' });
           }
         }
       ]
